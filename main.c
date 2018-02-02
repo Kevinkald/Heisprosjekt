a0c3f0f365810fbb494f4d62c2a2ef1b63a35e03
Kevin Kaldvansvik <kevinkald@hotmail.com>
2018-02-02 13:14:43 +0100

    fixing shit in main.c

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
 int main(){
-<<<<<<< HEAD
-	
 	printf("(Press key to continue: )\n");
 	char c;
 	char k = 'k';
@@ -14,12 +12,10 @@ int main(){
 	while(c!=k) {
 		printf("press k to stop!!\n");
 		c=getchar( );
-=======
 	printf("Hello Kevin\n");
 	printf("This is Jan <3\n");
 	if (1>0) {
 		return 1;
->>>>>>> 2406b8e08d93b21b6990d5d3ab5490408a4a5493
 	}
 	printf("Program done, terminating program.\n");
 	return 0;
